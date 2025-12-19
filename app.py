a11b9c378210e254afaa590fdaa7dd436169d960
harisudhan2025 <crimeninja2022@gmail.com>
2025-12-20 01:16:20 +0530

    Update app for Render deployment

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def index():
     return render_template('index.html', translation=translation, error=error)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=10000)
